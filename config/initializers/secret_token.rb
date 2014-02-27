de889fea50f2798eb52ff93244fc26bb7ae697db
Dino Poblete <dinopoblete@Serah.local>
2014-02-27 16:22:38 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BfhcfApp::Application.config.secret_key_base = '7d43995200dae20ebddf365cb6748c45dc64792da63cb882c78b96523e98dd8eb9ed4ff715a24ed750e2f08f430e738acdeb0363a8b60376983a0ab597e393c1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BfhcfApp::Application.config.session_store :cookie_store, key: '_bfhcf-app_session'
