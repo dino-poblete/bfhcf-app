5f86cd7232ac15d7f43ff05f4b5a8029e9721c46
Dino Poblete <dinopoblete@Serah.local>
2014-02-27 17:04:41 +0800

    added root index

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 BfhcfApp::Application.routes.draw do
+  get "root/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+
+  resource :posts
+
+  root 'root#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
